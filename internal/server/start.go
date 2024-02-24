bc1b233cbcdbc48b3df9715606230d6b631930e1
offlaneDefender <116073804+offlaneDefender@users.noreply.github.com>
2024-02-24 16:32:42 +0200

    Moved InMemory ProgressTracker to repo layer (#8)

--- internal/server/start.go
+++ internal/server/start.go
@@ -2,89 +2,23 @@ package server
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"math"
 	"net/http"
-	"slices"
 
 	"github.com/offlaneDefender/progress-tracker-go/internal/common"
+	"github.com/offlaneDefender/progress-tracker-go/internal/server/repo"
 )
 
 type goal = common.Goal
 
-type ProgressTracker struct {
-	Goals []goal
-}
-
-func (pt *ProgressTracker) AddGoal(name string, maxTicks int) error {
-	if maxTicks <= 0 {
-		return errors.New("MaxTicks cannot be less than 1")
-	}
-	pt.Goals = append(pt.Goals, goal{Name: name, Progress: 0, MaxTicks: maxTicks})
-
-	return nil
-}
-
-func (pt *ProgressTracker) TickProgress(name string) (float64, error) {
-	index := pt.FindByName(name)
-	if index == -1 {
-		err := fmt.Sprintf("Cannot find goal %v", name)
-		return -1, errors.New(err)
-	}
-
-	foundGoal := pt.Goals[index]
-
-	if foundGoal.MaxTicks == 0 {
-		return -1, errors.New("malformed data, MaxTicks 0")
-	}
-
-	tickrate := float64(100 / foundGoal.MaxTicks)
-
-	/*	floating point numbers are fun!
-		chose 0.005 as close-enough to zero
-		might 'increase' precision ( add more zeros ) later if needed
-	*/
-	if foundGoal.Complete ||
-		math.Abs(foundGoal.Progress-100) < 0.005 ||
-		math.Abs(foundGoal.Progress+tickrate-100) < 0.005 {
-		if foundGoal.Progress != 100 {
-			pt.Goals[index].Progress = 100
-		}
-		pt.Goals[index].Complete = true
-		return 100, nil
-	}
-
-	pt.Goals[index].Progress += tickrate
-
-	return pt.Goals[index].Progress, nil
-}
-
-func (pt *ProgressTracker) DeleteGoal(name string) (bool, error) {
-	index := pt.FindByName(name)
-	if index == -1 {
-		err := fmt.Sprintf("Cannot find goal %v", name)
-		return false, errors.New(err)
-	}
-
-	pt.Goals[index] = pt.Goals[len(pt.Goals)-1]
-	pt.Goals = pt.Goals[:len(pt.Goals)-1]
-
-	return true, nil
-}
-
-func (pt *ProgressTracker) FindByName(name string) int {
-	return slices.IndexFunc(pt.Goals, func(g goal) bool { return g.Name == name })
-}
-
 func Start() {
-	pt := ProgressTracker{Goals: []goal{}}
+	pt := repo.CreateProgressTracker()
 
 	// Initialize a server
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Request received from", r.RemoteAddr, "for", r.URL, "with method", r.Method)
 
-		fmt.Fprintf(w, "Goals: %v", pt.Goals)
+		fmt.Fprintf(w, "Goals: %v", pt.ReadGoals())
 	})
 
 	http.HandleFunc("PUT /", func(w http.ResponseWriter, r *http.Request) {
@@ -129,7 +63,7 @@ func Start() {
 			http.Error(w, err.Error(), 400)
 		}
 
-		fmt.Fprintf(w, "Goals: %v", pt.Goals)
+		fmt.Fprintf(w, "Goals: %v", pt.ReadGoals())
 	})
 	http.HandleFunc("DELETE /", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Request received from", r.RemoteAddr, "for", r.URL, "with method", r.Method)
